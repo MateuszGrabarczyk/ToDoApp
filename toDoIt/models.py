f5c925828111746e3b5fd830c9d4dcf42f12efe4
MateuszGrabarczyk <mategra240@student.polsl.pl>
2023-03-07 12:04:46 +0100

    - Created Task model

--- toDoIt/models.py
+++ toDoIt/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    active = models.BooleanField(default=True)
+    class Meta:
+        verbose_name = ("Task")
+        verbose_name_plural = ("Tasks")
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("Task_detail", kwargs={"pk": self.pk})
