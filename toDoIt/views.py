50b9071b9ded3d03420294cf96060612ce2a03d5
MateuszGrabarczyk <mategra240@student.polsl.pl>
2023-03-07 16:23:50 +0100

    - Prepared basic styling for the app

--- toDoIt/views.py
+++ toDoIt/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from toDoIt.models import Task
+
 
 def index(request):
-    return render(request, "toDoIt/index.html")
+    tasks = Task.objects.all()
+    return render(request, "toDoIt/index.html", {'tasks':tasks})
