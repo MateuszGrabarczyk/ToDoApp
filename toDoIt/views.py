0857fe1a86787e941891b76c078dfff34a8facd9
MateuszGrabarczyk <mategra240@student.polsl.pl>
2023-03-08 09:00:29 +0100

    - Prepared views for CRUD operations

--- toDoIt/views.py
+++ toDoIt/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from toDoIt.models import Task
 
 
 def index(request):
     tasks = Task.objects.all()
     return render(request, "toDoIt/index.html", {'tasks':tasks})
+
+class TaskDetail(DetailView):
+	model = Task
+
+class TaskCreate(CreateView):
+	model = Task
+	fields = ['title', 'description', 'active']
+	success_url = reverse_lazy('')
+
+class TaskUpdate(UpdateView):
+	model = Task
+	fields = ['title', 'description', 'active']
+	success_url = reverse_lazy('')
+
+class TaskDelete(DeleteView):
+	model = Task
+	context_object_name = 'task'
+	success_url = reverse_lazy('')
