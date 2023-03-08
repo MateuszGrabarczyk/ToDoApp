0bf805f8d83276f24c57337e6f3bcf953313a7e4
MateuszGrabarczyk <mategra240@student.polsl.pl>
2023-03-08 09:04:12 +0100

    - Prepared urls for CRUD operations

--- toDoIt/urls.py
+++ toDoIt/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-
+from .views import TaskDetail, TaskCreate, TaskUpdate, TaskDelete
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('tasks', views.index, name='index'),
+    path('task/<str:pk>/', TaskDetail.as_view(), name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<str:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<str:pk>/', TaskDelete.as_view(), name='task-delete'),
 ]
